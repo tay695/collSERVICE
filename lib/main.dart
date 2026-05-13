dbc14232c3bc91d2e61dcbc8fa33358244d5d337
maria-clara <mc.macie@outlook>
2026-05-13 08:06:42 -0300

    Adicionado main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,36 @@
 
+import 'package:coolservice/presentation/funcionarios/view/funcionario_list_page.dart';
+import 'package:coolservice/presentation/funcionarios/view_model/funcionario_viewModel.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'data/repositories/sqlite_funcionario_repository.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider(create: (_) => SQLiteFuncionarioRepository()),
+        ChangeNotifierProvider(
+          create: (context) =>
+              FuncionarioViewModel(context.read<SQLiteFuncionarioRepository>())
+                ..listAll(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CoolService',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const FuncionarioListPage(),
+    );
+  }
+}
